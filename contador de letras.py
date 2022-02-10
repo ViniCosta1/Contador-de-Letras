c9cf604753f07ac79cfbcc8dbe916f41622a6642
ViniCosta <53050163+ViniCosta1@users.noreply.github.com>
2022-02-10 08:51:09 -0300

    Add files via upload

---
+++ contador de letras.py
@@ -0,0 +1,9 @@
+import string
+alphabet = (string.ascii_lowercase)
+aLPHABET = (string.ascii_uppercase)
+
+def contadorLetras(y):
+    for letters in alphabet:
+        print(f"Quantidade de \"{letters}\" na frase: {y.count(letters)} letra(s)")
+
+contadorLetras("ksajdlçakjdals")
