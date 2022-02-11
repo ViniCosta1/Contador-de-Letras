44fd092e3d19fb8108e87465a7877fe91c3f6176
ViniCosta <53050163+ViniCosta1@users.noreply.github.com>
2022-02-11 15:57:22 -0300

    Update contador de letras.py

--- contador de letras.py
+++ contador de letras.py
@@ -4,6 +4,11 @@ aLPHABET = (string.ascii_uppercase)
 
 def contadorLetras(y):
     for letters in alphabet:
-        print(f"Quantidade de \"{letters}\" na frase: {y.count(letters)} letra(s)")
-
+        if y.count(letters) > 0:
+            print(f"Quantidade de \"{letters}\" na frase: {y.count(letters)} letra(s)")
+    print()
+    print("ATENÇÃO!!!")
+    print("Esse programa NÃO contabiliza letras com acentos")
+    print("Nem números")
+    print("Ex.: 'é', 'ç', '12', entre outros...")
 contadorLetras(input("Digite sua Frase: "))
